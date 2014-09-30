e45665c3d26db3b38a20fecdc81c9b54223a0b3e
Yifit <yifit.b@gmail.com>
2014-09-30 14:44:54 +0200

    version test

---
+++ src/Article.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Poste8
+ */
+public class Article ()
+    { super();
+    }
+public Article(int reference)
+    {this();
+    setReference(reference);
+    }
+public Article(int reference, String designation)
+    {
+
+    }
